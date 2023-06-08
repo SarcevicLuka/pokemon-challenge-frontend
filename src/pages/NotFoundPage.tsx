7eb06ff3e7190527cf95569995e34388d6d3e43b
SarcevicLuka <luka.sarac99@gmail.com>
2023-06-08 17:09:09 +0200

    Bit of styling of 404 page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,6 +1,12 @@
+import { AvailableRoutes } from "../routes/AvailableRoutes";
+
 const NotFoundPage = () => {
     return (
-        <div >Not found</div>
+        <div className="d-flex flex-column align-items-center justify-content-center">
+            <p>404: Page not found</p>
+            <p>You are accessing a url that does not exist or are not authorized to access this page</p>
+            <a href={AvailableRoutes.Home}>Go home</a>
+        </div>
     )
 }
 
